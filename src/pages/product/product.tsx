13fe723b5f2c3e42144ef33284ccc55051896d87
Ksenia <ksenechka.93@gmail.com>
2023-06-07 11:42:52 +0300

    fix: modal behavior

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -8,6 +8,7 @@ import { Camera } from '../../types/camera';
 import Footer from '../../components/footer/footer';
 import { getCameraInfoAction, getReviewsAction, getSimilarProductsAction } from '../../store/api-actions';
 import { getProduct } from '../../store/product/product.selectors';
+import { getSimilarProducts } from '../../store/similar-products/similar-products.selectors';
 import Header from '../../components/header/header';
 import ModalProductReview from '../../components/modal-product-review/modal-product-review';
 import ModalProductReviewSuccess from '../../components/modal-product-review-success/modal-product-review-success';
@@ -15,7 +16,6 @@ import ProductInfo from '../../components/product-info/product-info';
 import ProductSimilar from '../../components/product-similar/product-similar';
 import ReviewBlock from '../../components/review-block/review-block';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { getSimilarProducts } from '../../store/similar-products/similar-products.selectors';
 
 function Product(): JSX.Element {
   const id = useParams().id;
@@ -42,19 +42,29 @@ function Product(): JSX.Element {
       behavior: 'smooth'
     });
   };
-  //TODO: двойной esc в модалках
-  const handleAddReviewModalShow = useCallback((camera: Camera | null) => {
-    document.body.style.overflow = isAddReviewModalOpened ? '' : 'hidden';
 
-    setAddReviewModalOpened(!isAddReviewModalOpened);
+
+  const onAddReviewModalShow = useCallback(((camera: Camera | null) => {
+    document.body.style.overflow = '';
+    setAddReviewModalOpened(true);
     setProduct(camera);
-  }, [isAddReviewModalOpened]);
+  }),[setAddReviewModalOpened]);
 
-  const handleAddReviewSuccess = useCallback(() => {
-    document.body.style.overflow = isAddReviewSuccessModalOpened ? '' : 'hidden';
+  const onAddReviewModalHide = useCallback(() => {
+    document.body.style.overflow = 'hidden';
+    setAddReviewModalOpened(false);
+  },[setAddReviewModalOpened]);
 
-    setAddReviewSuccessModalOpened(!isAddReviewSuccessModalOpened);
-  },[isAddReviewSuccessModalOpened]);
+  const onAddReviewModalSuccessShow = useCallback(() => {
+    document.body.style.overflow = '';
+    setAddReviewSuccessModalOpened(true);
+  },[setAddReviewSuccessModalOpened]);
+
+  const onAddReviewModalSuccessHide = useCallback(() => {
+    document.body.style.overflow = 'hidden';
+    setAddReviewSuccessModalOpened(false);
+  },[setAddReviewSuccessModalOpened]);
+
 
   return (
     <>
@@ -73,21 +83,21 @@ function Product(): JSX.Element {
               {similarProducts.length && <ProductSimilar products={similarProducts} />}
             </div>
             <div className="page-content__section">
-              <ReviewBlock camera={choosedProduct as Camera} onAddReviewButtonClick={handleAddReviewModalShow}/>
+              <ReviewBlock camera={choosedProduct as Camera} onAddReviewButtonClick={onAddReviewModalShow}/>
             </div>
           </div>
           <ReactFocusLock disabled={!isAddReviewModalOpened} returnFocus>
             <ModalProductReview
               isOpened={isAddReviewModalOpened}
               product={product}
-              onCloseButtonClick={handleAddReviewModalShow}
-              onAddReviewSuccess={handleAddReviewSuccess}
+              onCloseButtonClick={onAddReviewModalHide}
+              onAddReviewSuccess={onAddReviewModalSuccessShow}
             />
           </ReactFocusLock>
           <ReactFocusLock disabled={!isAddReviewSuccessModalOpened} returnFocus>
             <ModalProductReviewSuccess
               isOpened={isAddReviewSuccessModalOpened}
-              onCloseButtonClick={handleAddReviewSuccess}
+              onCloseButtonClick={onAddReviewModalSuccessHide}
             />
           </ReactFocusLock>
         </main>
