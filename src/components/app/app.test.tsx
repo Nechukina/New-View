95bf8fd6dbcbb15d8c11b4f5098b02d98dd64484
Ksenia <ksenechka.93@gmail.com>
2023-07-15 14:28:04 +0300

    check after installing nvm

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -8,7 +8,6 @@ import { createMockStore} from '../../utils/mocks';
 import { Provider } from 'react-redux';
 import { createMockStoreWithAPI } from '../../utils/jest';
 
-
 const store = createMockStore();
 const { fakeStore } = createMockStoreWithAPI(store);
 
